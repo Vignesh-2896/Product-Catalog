49840c09a93466e5f7e571487d323854ee3c3725
Vignesh <vigneshb2896@gmail.com>
2024-09-28 18:25:15 +0530

    Formatting and Corrections.

--- generator.js
+++ generator.js
@@ -1,53 +1,50 @@
-(function(window)
-{
-var types = ['Games','Books','Electronics','Snacks & Drinks','Fruits & Veggies'];
-var price=0;
-var type='';
-setHeader("list_prod");
-setHeader("list_prod_sim");
-for(var i=0;i<100;i++)
-{
-	price=(Math.random()*500).toFixed(2);
-	type=types[Math.floor(Math.random()*5)];
-	tr = document.createElement("tr");
-	tr.setAttribute("id",i+1);
-	var td1 = document.createElement("td");
-	var td2 = document.createElement("td");
-	td2.setAttribute("id","type_"+(i+1));
-	var td3 = document.createElement("td");
-	td3.setAttribute("id","price_"+(i+1));
-	var td4 = document.createElement("td");	
-	var examine = document.createElement("button");
-	examine.setAttribute('onclick',"examine("+(i+1)+")");
-	
-	td1.innerHTML= i + 1;
-	td2.innerHTML = type;
-	td3.innerHTML = price;
-	examine.innerHTML = "Examine";
-	td4.appendChild(examine);
-	tr.appendChild(td1);
-	tr.appendChild(td2);
-	tr.appendChild(td3);
-	tr.appendChild(td4);
-	
-	document.getElementById("list_prod").appendChild(tr);
-}	
+(function (window) {
+	let types = ['Games', 'Books', 'Electronics', 'Snacks & Drinks', 'Fruits & Veggies'];
+	let price = 0;
+	let type = '';
+	setHeader("list_prod");
+	setHeader("list_prod_sim");
+	for (let i = 0; i < 100; i++) {
+		price = (Math.random() * 500).toFixed(2);
+		type = types[Math.floor(Math.random() * 5)];
+		let tr = document.createElement("tr");
+		tr.setAttribute("id", i + 1);
+		let td1 = document.createElement("td");
+		let td2 = document.createElement("td");
+		td2.setAttribute("id", "type_" + (i + 1));
+		let td3 = document.createElement("td");
+		td3.setAttribute("id", "price_" + (i + 1));
+		let td4 = document.createElement("td");
+		let examine = document.createElement("button");
+		examine.setAttribute('onclick', "examine(" + (i + 1) + ")");
 
-function setHeader(div_id)
-{
-	var tr = document.createElement("tr");
-	var th1 = document.createElement("th");
-	var th2 = document.createElement("th");
-	var th3 = document.createElement("th");
-	var th4 = document.createElement("th");
-	th1.innerHTML = "Product ID";
-	th2.innerHTML = "Product Type";
-	th3.innerHTML = "Product Price";
-	th4.innerHTML = "Examine";
-	tr.appendChild(th1);
-	tr.appendChild(th2);
-	tr.appendChild(th3);
-	tr.appendChild(th4);
-	document.getElementById(div_id).appendChild(tr);	
-}
+		td1.innerHTML = i + 1;
+		td2.innerHTML = type;
+		td3.innerHTML = price;
+		examine.innerHTML = "Examine";
+		td4.appendChild(examine);
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tr.appendChild(td3);
+		tr.appendChild(td4);
+
+		document.getElementById("list_prod").appendChild(tr);
+	}
+
+	function setHeader(div_id) {
+		let tr = document.createElement("tr");
+		let th1 = document.createElement("th");
+		let th2 = document.createElement("th");
+		let th3 = document.createElement("th");
+		let th4 = document.createElement("th");
+		th1.innerHTML = "Product ID";
+		th2.innerHTML = "Product Type";
+		th3.innerHTML = "Product Price";
+		th4.innerHTML = "Examine";
+		tr.appendChild(th1);
+		tr.appendChild(th2);
+		tr.appendChild(th3);
+		tr.appendChild(th4);
+		document.getElementById(div_id).appendChild(tr);
+	}
 })(window);
--- worker.js
+++ worker.js
@@ -1,91 +1,78 @@
-function examine(id)
-{
-	var promise = new Promise(function(resolve,reject)
-	{
-		if(id> 0 && id <= 100)
+function examine(id) {
+	let promise = new Promise(function (resolve, reject) {
+		if (id > 0 && id <= 100)
 			resolve(id);
 		else
 			reject("ID Does Not Exist. Try Again.");
 	});
-	var promise2 = promise.then(function(result)
-	{
-	document.getElementById("product_id").innerHTML = document.getElementById(id).id;
-	document.getElementById("product_price").innerHTML = document.getElementById("price_"+result).innerHTML;
-	document.getElementById("product_type").innerHTML = document.getElementById("type_"+result).innerText;
-	return result;
+	let promise2 = promise.then(function (result) {
+		document.getElementById("product_id").innerHTML = document.getElementById(id).id;
+		document.getElementById("product_price").innerHTML = document.getElementById("price_" + result).innerHTML;
+		document.getElementById("product_type").innerHTML = document.getElementById("type_" + result).innerText;
+		return result;
 	});
-	promise2.then(function(result)
-	{
+	promise2.then(function (result) {
 		deletesimilar();
-		for (var i = 1 ; i<=100; i++)
-		{
-			if(i != result)
-			{
-				if(document.getElementById("type_"+result).innerHTML == document.getElementById("type_"+i).innerHTML)
-				{
+		for (let i = 1; i <= 100; i++) {
+			if (i != result) {
+				if (document.getElementById("type_" + result).innerHTML == document.getElementById("type_" + i).innerHTML) {
 					similar(i);
 				}
 			}
 		}
 	});
 }
-function search()
-{
-	var promise = new Promise(function(resolve,reject)
-	{
-		var id_val = document.getElementById("search_bar").value;
-		if(id_val > 0 && id_val<=100)
+function search() {
+	let promise = new Promise(function (resolve, reject) {
+		let id_val = document.getElementById("search_bar").value;
+		if (id_val > 0 && id_val <= 100)
 			resolve(id_val);
 		else
 			reject("Failure");
 	});
-	
-	var promise2 = promise.then(function(result){
+
+	let promise2 = promise.then(function (result) {
 		return result;
-	}).catch(function(err)
-	{
+	}).catch(function (err) {
 		alert("ID Does not Exist. Try Again");
 	});
-	promise2.then(function(result)
-	{
+	promise2.then(function (result) {
 		examine(result);
 	});
 }
-function similar(id_val)
-{	
-	var tr = document.createElement("tr");
-	var td1 = document.createElement("td");
-	var td2 = document.createElement("td");
-	var td3 = document.createElement("td");
-	var td4 = document.createElement("td");
-	var td_button = document.createElement("button");
-	
-	tr.setAttribute("id",id_val);
+function similar(id_val) {
+	let tr = document.createElement("tr");
+	let td1 = document.createElement("td");
+	let td2 = document.createElement("td");
+	let td3 = document.createElement("td");
+	let td4 = document.createElement("td");
+	let td_button = document.createElement("button");
+
+	tr.setAttribute("id", id_val);
 	td1.innerHTML = id_val;
-	td2.innerHTML = document.getElementById("type_"+id_val).innerHTML;
-	td3.innerHTML = document.getElementById("price_"+id_val).innerHTML;
-	td_button.innerHTML ="Examine";
-	td_button.setAttribute("onclick","examine("+id_val+")");
-	
+	td2.innerHTML = document.getElementById("type_" + id_val).innerHTML;
+	td3.innerHTML = document.getElementById("price_" + id_val).innerHTML;
+	td_button.innerHTML = "Examine";
+	td_button.setAttribute("onclick", "examine(" + id_val + ")");
+
 	td4.appendChild(td_button);
 	tr.appendChild(td1);
 	tr.appendChild(td2);
 	tr.appendChild(td3);
-	tr.appendChild(td4);	
+	tr.appendChild(td4);
 
 	document.getElementById("list_prod_sim").appendChild(tr);
 }
-function deletesimilar()
-{
-	var list_prod_sim  = document.getElementById("list_prod_sim");
-	while(list_prod_sim.firstChild)
+function deletesimilar() {
+	let list_prod_sim = document.getElementById("list_prod_sim");
+	while (list_prod_sim.firstChild)
 		list_prod_sim.removeChild(list_prod_sim.firstChild);
-	
-	var tr = document.createElement("tr");
-	var th1 = document.createElement("th");
-	var th2 = document.createElement("th");
-	var th3 = document.createElement("th");
-	var th4 = document.createElement("th");
+
+	let tr = document.createElement("tr");
+	let th1 = document.createElement("th");
+	let th2 = document.createElement("th");
+	let th3 = document.createElement("th");
+	let th4 = document.createElement("th");
 	th1.innerHTML = "Product ID";
 	th2.innerHTML = "Product Type";
 	th3.innerHTML = "Product Price";
